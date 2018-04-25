adb6ed4bb6116568a38aed650739d96b458008d3
hollylux <hollyluxl@gmail.com>
2018-04-25 13:00:43 -0400

    prepare for heroku deploy

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = new express();
+const PORT = process.env.PORT || 5000
 
 app.get("/hi", (req, res) => {
     res.send({Hi: "Bruce"})
 });
 
-app.listen(5000);
+app.listen(PORT);
