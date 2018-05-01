9a0c4902c0b584695fa27755d3b35806d0414066
hollylux <hollyluxl@gmail.com>
2018-05-01 15:20:08 -0400

    Adding session support

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
     googleClientID: "707589730805-cllg57s41cnr6sat7m2gmont3ad67spk.apps.googleusercontent.com",
     googleClientSecret: "lC8KrZ12hodk8CdVC5XhqO0b",
-    mongoURI: "mongodb://blue:b1ueM0ng0@ds263989.mlab.com:63989/bluemongo"
+    mongoURI: "mongodb://blue:b1ueM0ng0@ds263989.mlab.com:63989/bluemongo",
+    sessionKey: 'zsdfhjm%^&*('
 };
--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const keys = require("./config/keys");
 const mongoose = require("mongoose");
 const express = require("express");
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 const app = new express();
 require("./routes/authRoute")(app);
 require('./models/User');
 require("./services/passportService");
 
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    keys: [keys.sessionKey]
+}));
+
+app.use()
+
 mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000
-app.get("/hi", (req, res) => {
-    res.send({Hi: "Bruce"})
-});
-
 app.listen(PORT);
