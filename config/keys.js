11ea11f261da7e14ba6f5a4bc20599724fec9478
hollylux <hollyluxl@gmail.com>
2018-05-01 17:46:25 -0400

    Dev vs. prod

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-    googleClientID: "707589730805-cllg57s41cnr6sat7m2gmont3ad67spk.apps.googleusercontent.com",
-    googleClientSecret: "lC8KrZ12hodk8CdVC5XhqO0b",
-    mongoURI: "mongodb://blue:b1ueM0ng0@ds263989.mlab.com:63989/bluemongo",
-    sessionKey: 'zsdfhjm%^&*('
-};
+if (process.env.NODE_ENV === 'production'){
+    module.exports = require('./prod');
+}else{
+    module.exports = require('./dev');
+}
